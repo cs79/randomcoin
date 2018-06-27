2dba2875ce938b80626e8720b1fecbb4e1490525
Alex <cloudstrife79@gmail.com>
2018-06-27 10:21:13 -0400

    add events / modifiers / additional logic notes; minor bugfixes

--- contracts/RandomCoin.sol
+++ contracts/RandomCoin.sol
@@ -15,12 +15,25 @@ contract RandomCoin {
     // not sure if this one is needed: holders array lists all accounts who have pegged in and not closed out
     address[] holders;
 
+    // idea - 1 array of holders, 2 mappings (one for RDC balance, one for ETH sent to acquire it), OR mapping of address => struct (whichever is cheaper)
+    // this would allow iteration over addresses (holders array) and then reference / manipulation of values related to those addresses
+ 
     // declare events
     // do any of these need to be indexed ? any other thing we want to log ?
-    event PeggedIn(address _add, int256 _amt);
-    event PeggedOut(address _add, int256 _amt);
+    event PeggedIn(address _add, uint256 _amt);
+    event PeggedOut(address _add, uint256 _amt);
+    event TriggeredEquitableLiquidation(address _add);
+    event TriggeredEquitableDestruct();
 
     // declare modifiers
+    // modifier to check that call is internal (for payables that must be public, but should only be triggered by this contract's functions)
+    modifier isInternalCall(address _add) {
+        require(
+            msg.sender == address(this),
+            "Function must be called by this contract"
+        );
+        _;
+    }
 
     // declare constructor + other functions
     constructor() public
@@ -34,6 +47,8 @@ contract RandomCoin {
         // the most important piece -- will be called to generate the rate when pegIn() or pegOut() is called
         // needs to have an EV of 100
         // no idea what math / random libraries are already available in solidity... hopefully something I can work with for this 
+        // just assume that RANDAO is up and running for the time being; tackle random generation last ?
+        // can use insecure method hashing block data as a placeholder; replace "in production" w/ something like RANDAO
     }
 
     function pegIn(address _add, uint _amt) public payable
@@ -46,43 +61,59 @@ contract RandomCoin {
         rdcBalances[_add] = randomRate() * _amt;
         // append the address to holders if it doesn't already exist in the array (check if lookup in rdcBalances is default value before adding to mapping)
         if (!isHolderInMapping) {  // not sure on ! syntax here...
-            holders.push(_add)
+            holders.push(_add);
         }
         // emit the PeggedIn event
+        emit PeggedIn(_add, _amt);
     }
 
     function pegOut(address _add, uint _amt) public payable
     {
         // need to validate that _amt does not exceed rdcBalances[_addr]
         if (rdcBalances[_add] < _amt) {
-            revert()
+            revert();
         }
         // if it does not, send the random rate in reverse (may need to use floor division for now)
         uint toSend = _amt / randomRate();
         // need to think about what happens if this toSend amount would drain the balance of the contract
         // MAYBE -- equitableDestruct() to return something to everyone
         if (toSend > this.balance) {
-            equitableDestruct()
+            equitableDestruct();
         }
         // otherwise, send the toSend amount to _add
         rdcBalances[_add] = rdcBalances[_add] - _amt;
-        _add.send(toSend);
+        _add.transfer(toSend);
         // emit the PeggedOut event
+        emit PeggedOut(_add, _amt);
     }
 
-    function equitableDestruct() private payable
+    // can't return mapping ? need to declare this as a state variable and use this function to set the values?
+    // as in other cases, may need pattern of array + mapping here for later iteration
+    function calcPayouts() private returns(mapping(address => uint256))
+    {
+        // should be called by both equitableDestruct and equitableLiquidation
+
+    }
+
+    // can't be private -- if public, assert that msg.sender is this contract's address ?
+    function equitableDestruct() public payable isInternalCall(msg.sender)
     {
         // split up the ether in this contract's balance proportionally to RDC ownership among all addresses in holders
         // basically a version of equitableLiquidation() that can be called during pegOut if the pot would be drained, rather than on command
 
+        // emit relevant event
+        emit TriggeredEquitableDestruct();
     }
 
     function equitableLiquidation() public payable
     {
-        if msg.sender == owner:
+        if (msg.sender == owner) {
             // split up the ether in this contract's balance by proportional RDC balance of all keys
             // maybe this will not work, or addl data structure is required, since mappings cannot be iterated over (?)
             // could store a (potentially expensive) array of holders to identify the "valid" keys in the rdcBalances mapping
 
+            // emit relevant event
+            emit TriggeredEquitableLiquidation(msg.sender);
+        }
     }
 }
--- contracts/RandomLotto.sol
+++ contracts/RandomLotto.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.0;
 
 contract RandomLotto {
     // should be "round-based" with a minimum number of blocks to pass, plus delay if not enough participants have entered within that limit
+    // so, state machine, basically -- see: https://solidity.readthedocs.io/en/develop/common-patterns.html?#state-machine
     // additional function to withdraw your bet should be allowed if the number of blocks have passed but no one else has joined
     // (could just send back after X blocks, but should incentivize staying in)
     address owner;
@@ -9,9 +10,17 @@ contract RandomLotto {
     mapping (address => uint) ticketBalances;
     address[] ticketHolders;  // not sure if necessary ?
     uint lastDraw;
+    enum States {
+        SellingTickets,
+        DrawingWinner,
+        PayingOut
+    }
     // idea from Medium post here: https://medium.com/@promentol/lottery-smart-contract-can-we-generate-random-numbers-in-solidity-4f586a152b27
     // use a "state" state variable for accepting tickets / drawing the value
     // this would probably be good in general
+
+    // events - will depend on final state varialbes, which need more thought at this point
+    event TriggeredEquitableLiquidation(address _add);
 
     constructor() public
     {
@@ -42,7 +51,26 @@ contract RandomLotto {
         // return the winning address
     }
 
-    function 
+    // ideally would return a mapping, but may need to just set one
+    // now that i think about this, probably requires both array and mapping since mapping keys can't be iterated over
+    function calcPayouts()
+    {
+
+    }
+
+    function sendPayouts()
+    {
+        // calls calcPayouts and then sends the appropriate balances to the addresses in the mapping
+    }
 
     // have some type of closeout by owner that returns balances
+    function equitableLiquidation()
+    {
+        if (msg.sender == owner) {
+            // equitable return
+
+            // emit relevant event
+            emit TriggeredEquitableLiquidation(msg.sender);
+        }
+    }
 }

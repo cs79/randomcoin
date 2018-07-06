34039a1ad3fe4f281fa7bf17ef105f78862e15fd
Alex <cloudstrife79@gmail.com>
2018-07-06 18:17:19 -0400

    add initial state management code

--- contracts/RandomCoin.sol
+++ contracts/RandomCoin.sol
@@ -9,35 +9,24 @@ import "./IBFactory.sol";
 
 contract RandomCoin {
     // declare state / storage variables
-    // maybe another contract type is needed for RDC accounts, individually owned ? or can a mapping handle this fine here?
     address owner;  // for recovery, but make sure it can't do anything weird to pegged in balances
+
     // averageRate should have an expected value of 100  (1, ideally, but no floats)
     // an "ideal" version of this would allow for timeseries graphing of average rate in the web service -- not sure how this might be achieved though
     uint averageRate;  // update this as events are emitted ? or rely on web service to aggregate records later?
+    
     // need to keep track of everyone who has pegged in
     // does "randomcoin" need to be an actual "token"? should it be? I guess that is more interesting tbh... learn about how to do this
     // (alternative is simply using a mapping as the sole arbiter of balances; all "trading" done with the contract only via peg-in / peg-out)
-    //mapping (address => uint) rdcBalances;
-    // replace with:
     IBFactory ibf;
     IterableBalances rdcBalances;  // I want this to be a new instance every time the state recycles
-    // not sure if this one is needed: holders array lists all accounts who have pegged in and not closed out
-    //address[] holders;
 
+    // use (IB.balances[add] / IB.totalBalance) * availablePayout to assign equitable balances to holders
     uint availablePayout;  // set to the CURRENT BALANCE of ether at this contract when an equitable liquidation occurs
-
-    // with updates to IterableBalances.sol, this should no longer be needed
-    // just use (IB.balances[add] / IB.totalBalance) * availablePayout
-    //mapping (address => uint) equitablePayouts;  // use this to assign equitable balances to holders
-
-    // struct for iterable holders - import from base contract; use instead of rdcBalances / holders
-    
-    // idea - 1 array of holders, 2 mappings (one for RDC balance, one for ETH sent to acquire it), OR mapping of address => struct (whichever is cheaper)
-    // this would allow iteration over addresses (holders array) and then reference / manipulation of values related to those addresses
- 
 
     // state management
     enum State { Funding, Active, Liquidating }
+    State state;
 
     // declare events
     // do any of these need to be indexed ? any other thing we want to log ?
@@ -46,6 +35,9 @@ contract RandomCoin {
     event PeggedOut(address _add, uint256 _amt);
     event TriggeredEquitableLiquidation(address _add);
     event TriggeredEquitableDestruct();
+    event StateChangeToFunding();
+    event StateChangeToActive();
+    event StateChangeToLiquidating();
 
     // declare modifiers
     // modifier to check that call is internal (for payables that must be public, but should only be triggered by this contract's functions)
@@ -57,6 +49,28 @@ contract RandomCoin {
         _;
     }
 
+    // need state-checking modifiers as well
+    modifier notLiquidating() {
+        require(
+            state != State.Liquidating
+        );
+        _;
+    }
+
+    modifier stateIsActive() {
+        require(
+            state == State.Active
+        );
+        _;
+    }
+
+    modifier canWithdrawEquitably() {
+        require(
+            state == State.Liquidating
+        );
+        _;
+    }
+
     // declare constructor + other functions
     constructor() public
     {
@@ -64,6 +78,7 @@ contract RandomCoin {
         averageRate = 100;  // since there are no floats yet, index to 100 instead of 1
         ibf = IBFactory(this);
         rdcBalances = ibf.createIB();
+        state = State.Funding;
     }
 
     function randomRate()
@@ -78,9 +93,10 @@ contract RandomCoin {
         // can use insecure method hashing block data as a placeholder; replace "in production" w/ something like RANDAO
     }
 
-    function pegIn(uint _amt)
+    function pegIn()
     public
     payable
+    notLiquidating()
     {
         
         // logic for checking whether holder is in index is now in IterableBalances.sol
@@ -95,6 +111,7 @@ contract RandomCoin {
     function pegOut(uint _amt)
     public
     payable
+    stateIsActive()
     {
         // logic for checking randomcoin balance has been moved to IterableBalances.sol
         address _add = msg.sender;
@@ -112,40 +129,60 @@ contract RandomCoin {
         emit PeggedOut(_add, _amt);
     }
 
-    // can't return mapping ? need to declare this as a state variable and use this function to set the values?
-    // as in other cases, may need pattern of array + mapping here for later iteration
-    function calcPayouts()
-    private
+    function equitableWithdrawal()  // maybe rename this...
+    public
+    payable
+    canWithdrawEquitably()
     {
-        // should be called by both equitableDestruct and equitableLiquidation
-        // modify the mapping equitablePayouts based on calculation
-
-        // this is actually no longer needed - need a "withdrawFairShare" type fn for when equitable destruct type fn
-        // has changed state to Liquidating
+        address _add = msg.sender;
+        uint _payout = (rdcBalances.balances(_add) / rdcBalances.totalBalance()) * availablePayout;
+        _add.transfer(_payout);
+        // may need to handle the case where the last person to withdraw cannot do so because fees have drained what would have been proportional shares initially
     }
 
     // can't be private -- if public, assert that msg.sender is this contract's address ?
-    function equitableDestruct() public payable isInternalCall(msg.sender)
+    function equitableDestruct()
+    public
+    payable
+    isInternalCall(msg.sender)
+    notLiquidating()
     {
-        // split up the ether in this contract's balance proportionally to RDC ownership among all addresses in holders
-        // basically a version of equitableLiquidation() that can be called during pegOut if the pot would be drained, rather than on command
+        // set state to Liquidating
+        startLiquidation();
+        // probably need some sort of "startCountdown()" function to get called here which allows for withdrawal within a particular window
+        // (window also should then be a state variable -- maybe this can be changed by owner, but ONLY within certain [reasonable] limits)
 
-        // this may be infeasible if entire function call must be executed within one block -- could attempt this, but revert if failed to "fallback state"
-        // or could simply flag the whole contract as being in a particular state once this is called, and while in that state, allow withdrawal only for "equitable calcs"
-
-        // emit relevant event
+        // emit relevant events
         emit TriggeredEquitableDestruct();
     }
 
-    function equitableLiquidation() public payable
+    function equitableLiquidation()
+    public
+    payable
+    notLiquidating()
     {
         if (msg.sender == owner) {
-            // split up the ether in this contract's balance by proportional RDC balance of all keys
-            // maybe this will not work, or addl data structure is required, since mappings cannot be iterated over (?)
-            // could store a (potentially expensive) array of holders to identify the "valid" keys in the rdcBalances mapping
+            // set state to Liquidating
+            startLiquidation();
 
-            // emit relevant event
+            // emit relevant events
             emit TriggeredEquitableLiquidation(msg.sender);
         }
+        else {
+            revert();  // unnecessary ?
+        }
+    }
+
+    // instead of just manually changing the state in equitableDestruct / equitableLiquidation, use a smarter method here:
+    function startLiquidation()
+    private
+    {
+        // any modifiers needed here ?
+        state = State.Liquidating;
+        emit StateChangeToLiquidating();
+
+        // how can we start a timer and then "ensure" that the contract gets reset to funding state afterwards?
+        // this may not be directly possible, so can we make a modifier for ALL other functions that resets the state if eligible to do so ?
+
     }
 }
